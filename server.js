04c40d60f8e33d2edae310de5659761b4bcd7502
Ernesto Garcia <egarcia2020@yahoo.com>
2021-11-07 21:31:07 -0800

    latest code

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-// DEPENDENCIES
-// Series of npm packages that we will use to give our server useful functionality
+// ***Worked together with Jesus Perea on this assigment*** //
 
-const express = require('express');
+//require variables go here//
+const express = require("express");
 
 const path = require("path");
 
@@ -9,34 +9,36 @@ const db = require("./db/db.json")
 
 const fs = require('fs')
 
-
-// EXPRESS CONFIGURATION
-// This sets up the basic properties for our express server
-
-// Tells node that we are creating an "express" server
 const app = express();
 
-// Sets an initial port. We"ll use this later in our listener
+//PORT environment // 
 const PORT = process.env.PORT || 3002;
 
-//const PORT = 8080;// //without Heroku server
-
-// Sets up the Express app to handle data parsing
+ 
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname,"public", 'notes.html')));
-app.get('*', (req, res) => res.sendFile(path.join(__dirname,"public", 'index.html')));
-
-// ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-
-// LISTENER
-// The below code effectively "starts" our server
-
-app.listen(PORT, () => {
-  console.log(`App listening on PORT: http://localhost:${PORT}`);
+app.get('/api/notes', (req, res) => {
+  const notes = JSON.parse(fs.readFileSync("./db/db.json"))
+res.json(notes)
 });
+
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  const notes = JSON.parse(fs.readFileSync("./db/db.json"))
+  console.log(newNote)
+    notes.push(newNote)
+    
+ //use fs to write to the dbjson file notes after pushing
+ fs.writeFileSync("./db/db.json", JSON.stringify(notes))
+ res.json(newNote)
+});
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, "public", 'notes.html')));
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, "public", 'index.html')));
+
+app.listen(PORT, () => console.log(`App listening on PORT http://localhost:${PORT}`));
